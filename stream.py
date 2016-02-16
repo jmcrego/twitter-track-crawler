a77e2449ae7b5b94f9d3ec8a69caf83d71e23d8b
Josep Crego <crego@systran.fr>
2016-02-16 12:12:57 +0100

    updates

--- stream.py
+++ stream.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
 from TwitterAPI import TwitterAPI
+import sys
 import json
-from pprint import pprint
+import argparse
 
-CONSUMER_KEY = 'KnzpHso9d7qXfXT72Y0sly360'
-CONSUMER_SECRET = 'mQbryk7RInrRp3QZuSkqH4U59H4NsWQj4X8pWFspajklbirCFS'
-ACCESS_TOKEN_KEY = '3288314879-pysLtV3l1CnMo3hvRWaGf1KggPQP2xr4SqqcsFy'
-ACCESS_TOKEN_SECRET = 'jSGfcnlJAGxZLfbx7CbjGFSDPHP4i7u0NA0UqsU8f60qO'
-api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+parser = argparse.ArgumentParser(description='Twitter streamer.')
+parser.add_argument('-k', '--keys', help='file with Twitter application keys (four keys in four lines)', action="store", required=True)
+parser.add_argument('-t', '--terms', help='file with term keywords (one line with comma-separated list of terms)', action="store", required=True)
+parser.add_argument('-l', '--lang', help='language', action="store", required=True)
+parser.add_argument('-v', '--verbose', help='verbose output', action='count')
+args = parser.parse_args()
 
-TERM_EN = 'start match,first half,second half,end match,extra time,foul,goal,substitution,injury,expulsion,penalty,red card,yellow card,eruption,earthquake,tsunami,flood,tornado,hurricane,typhoon,cyclone,blizzard,hailstorm,wildfire'
-#TERM_FR = 'début match,fin match,prolongation,faute,but,remplacement,blessure,penalty,carton jaune,expulsion,éruption,séisme,tsunami,inondation,tornade,ouragan,typhon,cyclone,tempête,grêle,incendie'
-#TERM_AR = 'مباراة,المرمى,تسديدة,فريق,ركنية,منتخب,دوري,كأس العالم,بركان,زلزال,تسونامي,عاصفة,اعصار,حريق,زوبعة,بركان,زلازل,اعاصير,زوابع,طوفان,فيضانات,فيضان'
+KEYS = [line.rstrip('\n') for line in open(args.keys)]
+if len(KEYS)!=4 : sys.exit('error: keys file should contain 4 entries')
+if args.verbose : print >> sys.stderr, "KEYS: \t"+"\n\t".join(KEYS)
 
-LANGS = {'en':1, 'fr':1, 'ar':1}
-LANG = 'en'
+TERMS = [line.rstrip('\n') for line in open(args.terms)]
+if len(TERMS)==0 or len(TERMS)>400 : sys.exit('error: terms file should contain [1,400) entries')
+if args.verbose : print >> sys.stderr, "TERMS:\t"+"\n\t".join(TERMS)
 
-r = api.request('statuses/filter', {'track': TERM_EN, 'language': LANG})
-#r = api.request('search/tweets', {'q': TERM})
+LANG = args.lang
+if LANG!='en' and LANG!='fr' and LANG!='ar' : sys.exit('error: allowed languages are {\'en\',\'fr\',\'ar\'}')
+if args.verbose : print >> sys.stderr, "LANG: \t"+LANG
 
-for item in r:
-    if 'retweeted_status' in item : continue
-    if not 'id_str' in item : continue
-#    if not 'lang' in item or not item['lang'] in LANGS : continue
-#    print(item['lang']+"\t"+item['id_str']+"\t"+item['created_at']+"\t"+item['user']['screen_name']+"\t"+item['text'])
-    print( json.dumps(item, ensure_ascii='True'))
-#    print( json.dumps(item, ensure_ascii='True', indent='\t'))
+#sys.exit('c\'est fini')
 
-
-
+api = TwitterAPI(KEYS[0],KEYS[1],KEYS[2],KEYS[3])
+r = api.request('statuses/filter', {'track': ",".join(TERMS), 'language': LANG})
+for tweet in r:
+    if not 'id_str' in tweet : continue
+    if 'retweeted_status' in tweet : continue
+    print( json.dumps(tweet, ensure_ascii='True', sort_keys=True, separators=(',',': ')))
+#    print( json.dumps(tweet, ensure_ascii='True', sort_keys=True, indent=1, separators=(',',': ')))
+#    sys.exit('c\'est fini')
