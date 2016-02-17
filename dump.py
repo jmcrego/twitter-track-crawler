9fc6988c8c8471bd0bed45685d53da53673a1915
Josep Crego <crego@systran.fr>
2016-02-17 13:20:28 +0100

    light changes

--- dump.py
+++ dump.py
@@ -3,27 +3,51 @@ import sys
 import json
 import argparse
 
-parser = argparse.ArgumentParser(description='Twitter streamer.')
+parser = argparse.ArgumentParser(description='Twitter dumper.')
 parser.add_argument("-v", help='verbose output', action="count", default=0)
+parser.add_argument('do', choices=['id', 'text', 'user', 'time', 'geo'], metavar='STRING', nargs='+', help='tweet field to dump {id, text, user, time, geo}')
 args = parser.parse_args()
 
-n=0
-nok=0
-
+n = 0
+nok = 0
 for line in sys.stdin :
     n += 1
     try :
-        item = json.loads(line)
+        tweet = json.loads(line)
     except Exception, e :
         if args.v : print >> sys.stderr, "error "+str(e)+": line="+str(n)
-    if 'retweeted_status' in item : continue
-    if not 'id_str' in item : continue
-    if not 'user' in item or not 'screen_name' in item['user'] : continue
-    item['text'] = item['text'].replace('\n', unicode('✪','utf-8'))
-    item['text'] = item['text'].replace('\r', unicode('✪','utf-8'))
-    item['text'] = item['text'].replace('\t', unicode('❂','utf-8'))
+        continue
+
+    if not 'id_str' in tweet : continue
+    if not 'created_at' in tweet : continue
+    if not 'text' in tweet : continue
+    if 'retweeted_status' in tweet : continue
+    if not 'user' in tweet or not 'screen_name' in tweet['user'] : continue
+
+    out = []
+    for do in args.do :
+        if do == 'geo' : 
+            geo = []
+#            if 'place' in tweet and 'id' in tweet['place'] : geo.append(tweet['place']['id'])
+#            if 'place' in tweet and 'full_name' in tweet['place'] : geo.append(tweet['place']['full_name'])
+#            if len(geo)==0 : geo.append('-')
+#            out.append(':'.join(geo))
+#OR
+#            if 'place' in tweet :
+#                if 'attributes' in tweet['place'] : del tweet['place']['attributes']
+#                if 'bounding_box' in tweet['place'] and 'type' in tweet['place']['bounding_box'] : del tweet['place']['bounding_box']['type']
+#            out.append(json.dumps( tweet['place'], ensure_ascii='True', sort_keys=True, separators=(',',': ') ) )
+        if do == 'text' : 
+            tweet['text'] = tweet['text'].replace('\n', unicode('✪','utf-8'))
+            tweet['text'] = tweet['text'].replace('\r', unicode('✪','utf-8'))
+            tweet['text'] = tweet['text'].replace('\t', unicode('❂','utf-8'))
+            out.append(tweet['text'])
+        if do == 'user' : out.append(tweet['user']['screen_name'])
+        if do == 'time' : out.append(tweet['created_at'])
+        if do == 'id' : out.append(tweet['id_str'])
+
     try : 
-        print((item['id_str']+"\t"+item['created_at']+"\t"+item['user']['screen_name']+"\t"+item['text']).encode('utf-8'))
+        print('\t'.join(out).encode('utf-8'))
         nok += 1
     except Exception, e : 
         if args.v : print >> sys.stderr, "error "+str(e)+": line="+str(n)
--- stream.py
+++ stream.py
@@ -5,10 +5,10 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser(description='Twitter streamer.')
-parser.add_argument("-v", action="count", default=0,                  help='verbose output')
-parser.add_argument('-l', action='store', choices=['en', 'fr', 'ar'], help='language')
-parser.add_argument('-k', action='store',                             help='file with Twitter keys (one key per line)')
-parser.add_argument('-t', action='store',                             help='file with term keywords (one term per line)')
+parser.add_argument("-v", action="count", default=0, help='verbose output')
+parser.add_argument('-l', action='store', help='language of tweets streamed (Ex: {en,fr,ar})')
+parser.add_argument('-k', action='store', help='file with Twitter keys (one key per line)')
+parser.add_argument('-t', action='store', help='file with term keywords (one term per line)')
 args = parser.parse_args()
 
 KEYS = [line.rstrip('\n') for line in open(args.k)]
@@ -20,21 +20,8 @@ if args.v : print >> sys.stderr, "TERMS:\t"+"\n\t".join(TERMS)
 LANG = args.l
 if args.v : print >> sys.stderr, "LANG: \t"+LANG
 
-n=0
-nok=0
-try : 
-    api = TwitterAPI(KEYS[0],KEYS[1],KEYS[2],KEYS[3])
-    r = api.request('statuses/filter', {'track': ",".join(TERMS), 'language': LANG})
-    for tweet in r:
-        n += 1
-        if not 'id_str' in tweet : continue
-        if not 'created_at' in tweet : continue
-        if not 'text' in tweet : continue
-        if 'retweeted_status' in tweet : continue
-        print( json.dumps( tweet, ensure_ascii='True', sort_keys=True, separators=(',',': ') ) )
-        nok += 1
-
-except Exception, e : 
-    if args.v : print >> sys.stderr, "error "+str(e)+": line="+str(n)
-
-sys.exit('Done! processed '+str(nok)+ ' out of '+str(n)+' tweets')
+api = TwitterAPI(KEYS[0],KEYS[1],KEYS[2],KEYS[3])
+r = api.request('statuses/filter', {'track': ",".join(TERMS), 'language': LANG})
+for tweet in r : 
+    print( json.dumps( tweet, ensure_ascii='True', sort_keys=True, separators=(',',': ') ) )
+    
